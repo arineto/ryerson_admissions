c69cce2123d7f4fcafe7198c2bcd942571f45fc6
Arimatea Neto <arineto30@gmail.com>
2014-06-27 12:46:10 -0400

    Campus Tour layout

--- src/com/p15media/ryersonuniversityadmissions/CampusTourActivity.java
+++ src/com/p15media/ryersonuniversityadmissions/CampusTourActivity.java
@@ -1,16 +1,18 @@
 package com.p15media.ryersonuniversityadmissions;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 
 public class CampusTourActivity extends Activity {
 
+	@SuppressLint("InlinedApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setTheme(android.R.style.Theme_DeviceDefault_Light);
 		setContentView(R.layout.activity_campus_tour);
-		
 	}
 
 	@Override
