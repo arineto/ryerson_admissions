595b30f6ad68007efe4c713c327f9ad5c1adf5aa
Arimatea Neto <arineto30@gmail.com>
2014-06-27 10:54:37 -0400

    Improve opening pdfs and cleaning project

--- src/com/p15media/ryersonuniversityadmissions/HomeActivity.java
+++ src/com/p15media/ryersonuniversityadmissions/HomeActivity.java
@@ -1,24 +1,18 @@
 package com.p15media.ryersonuniversityadmissions;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
 @SuppressLint("SetJavaScriptEnabled")
 public class HomeActivity extends Activity {
+	
+	private Intent intent;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,70 +28,38 @@ public class HomeActivity extends Activity {
 	}
 	
 	public void campus_tour(View view){
-		Intent myIntent = new Intent(view.getContext(), CampusTourActivity.class);
-        startActivityForResult(myIntent, 0);
+		intent = new Intent(view.getContext(), CampusTourActivity.class);
+        startActivityForResult(intent, 0);
 	}
 	
 	public void admissions_handbook(View view){
-		CopyReadAssets("admission.pdf", view.getContext());
-		
+		openPDF("admission.pdf");		
 	}
 	
 	public void international_guide(View view){
-		CopyReadAssets("international.pdf", view.getContext());
+		openPDF("international.pdf");
 	}
 	
 	public void photo_gallery(View view){
-		Intent myIntent = new Intent(view.getContext(), PhotoGalleryActivity.class);
-        startActivityForResult(myIntent, 0);
+		intent = new Intent(view.getContext(), PhotoGalleryActivity.class);
+        startActivityForResult(intent, 0);
 	}
 	
 	public void video_gallery(View view){
-		Intent myIntent = new Intent(view.getContext(), VideoGalleryActivity.class);
-        startActivityForResult(myIntent, 0);
+		intent = new Intent(view.getContext(), VideoGalleryActivity.class);
+        startActivityForResult(intent, 0);
 	}
 	
 	public void ryerson_blogs(View view){
 		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://why.ryerson.ca/")));
 	}
 	
-	private void CopyReadAssets(String name, Context context)
-    {
-		AssetManager assetManager = getAssets();
-
-        InputStream in = null;
-        OutputStream out = null;
-        File file = new File(getFilesDir(), name);
-        try
-        {
-            in = assetManager.open(name);
-            out = openFileOutput(file.getName(), context.MODE_WORLD_READABLE);
-
-            copyFile(in, out);
-            in.close();
-            in = null;
-            out.flush();
-            out.close();
-            out = null;
-        } catch (Exception e)
-        {
-            Log.e("tag", e.getMessage());
-        }
-
-        Intent intent = new Intent(Intent.ACTION_VIEW);
+	public void openPDF(String name){
+		File file = new File(getFilesDir(), name);
+		intent = new Intent(Intent.ACTION_VIEW);
         intent.setDataAndType(Uri.fromFile(file), "application/pdf");
         intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
         startActivity(intent);
-    }
-
-    private void copyFile(InputStream in, OutputStream out) throws IOException
-    {
-        byte[] buffer = new byte[1024];
-        int read;
-        while ((read = in.read(buffer)) != -1)
-        {
-            out.write(buffer, 0, read);
-        }
-    }
+	}
 
 }
