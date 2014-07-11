2f7639d1f553289e36fec3e5813a232101a7edb3
Arimatea Neto <arineto30@gmail.com>
2014-07-11 13:49:55 -0400

    Opening socia media on apps, missing snapchat

--- src/com/p15media/ryersonuniversityadmissions/ConnectActivity.java
+++ src/com/p15media/ryersonuniversityadmissions/ConnectActivity.java
@@ -1,6 +1,7 @@
 package com.p15media.ryersonuniversityadmissions;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -15,11 +16,23 @@ public class ConnectActivity extends Activity {
 	}
 	
 	public void openFacebook(View view){
-		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://facebook.com/RyersonUAR")));
+		try {
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://profile/118137451556523"));
+			intent.setPackage("com.facebook.katana");
+			startActivity(intent);
+		} catch (Exception e) {
+			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/RyersonUAR")));
+		}
 	}
 	
 	public void openInstagram(View view){
-		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://instagram.com/RyersonUAR")));
+	    try {
+	    	Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://instagram.com/_u/RyersonUAR"));
+	    	intent.setPackage("com.instagram.android");
+	        startActivity(intent);
+	    } catch (ActivityNotFoundException e) {
+	        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://instagram.com/RyersonUAR")));
+	    }
 	}
 	
 	public void openSnapchat(View view){
@@ -31,7 +44,12 @@ public class ConnectActivity extends Activity {
 	}
 	
 	public void openTwitter(View view){
-		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://twitter.com/RyersonUAR")));
+		try {
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?user_id=123575972"));
+			startActivity(intent);
+		}catch (Exception e) {
+			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://twitter.com/RyersonUAR")));
+		}
 	}
 	
 	public void openNews(View view){
