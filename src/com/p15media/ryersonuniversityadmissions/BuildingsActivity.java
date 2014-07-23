6711593291092a9edc33563ee20a5428ef5aced5
Arimatea Neto <arineto30@gmail.com>
2014-07-23 12:08:17 -0400

    Campus tour videos from youtube api

--- src/com/p15media/ryersonuniversityadmissions/BuildingsActivity.java
+++ src/com/p15media/ryersonuniversityadmissions/BuildingsActivity.java
@@ -1,45 +1,47 @@
 package com.p15media.ryersonuniversityadmissions;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
-import android.widget.MediaController;
-import android.widget.VideoView;
+import android.widget.Toast;
 
-public class BuildingsActivity extends Activity {
-	
-	private VideoView video;
+public class BuildingsActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
 	private ImageView image;
-	private MediaController controller;
-	private String path;
-	private int video_name;
+	private String video_name;
 	private int picture_name;
+	static private final String DEVELOPER_KEY = "AIzaSyBOdkeGbh6pBewgLJ2nz7D91oLShbXOclI";
 
-	@SuppressLint("NewApi")
-	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_buildings);
+		setContentView(R.layout.activity_buildings);
 		
-		video_name = getIntent().getExtras().getInt("video_name");
+		video_name = getIntent().getExtras().getString("video_name");
 		picture_name =  getIntent().getExtras().getInt("picture_name");
 		
-		final BitmapFactory.Options options = new BitmapFactory.Options();
+		YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+		youTubeView.initialize(DEVELOPER_KEY, this);
 		
+		final BitmapFactory.Options options = new BitmapFactory.Options();
 		image = (ImageView) findViewById(R.id.building_image);
 		image.setImageBitmap(new PictureBitmap(options).decodeSampledBitmapFromResource(getResources(), picture_name, 400, 400));
-		
-		video = (VideoView) findViewById(R.id.building_video);
-		path = "android.resource://" + getPackageName() + "/" + video_name;
-		video.setVideoURI(Uri.parse(path));
-		video.start();
-		
-		controller = new MediaController(this);
-		controller.setMediaPlayer(video);
-		video.setMediaController(controller);
-		
+	}
+	
+	@Override
+	public void onInitializationFailure(Provider provider, YouTubeInitializationResult error) {
+		Toast.makeText(this, "Oh no! "+error.toString(),
+		Toast.LENGTH_LONG).show();
+	}
+	
+	@Override
+	public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
+		if (!wasRestored) {
+			player.cueVideo(video_name);
+	    }
 	}
 }
--- src/com/p15media/ryersonuniversityadmissions/CampusTourActivity.java
+++ src/com/p15media/ryersonuniversityadmissions/CampusTourActivity.java
@@ -28,94 +28,94 @@ public class CampusTourActivity extends Activity {
 	}
 
 	public void openKerrHall(View view){
-		openActivity(view.getContext(), R.raw.kerr, R.raw.khw_picture);
+		openActivity(view.getContext(), "Rx4mqpVCHh4", R.raw.khw_picture);
 	}
 	
 	public void openRAC(View view){
-		openActivity(view.getContext(), R.raw.rac, R.raw.rac_picture);
+		openActivity(view.getContext(), "hzHALKNwPlc", R.raw.rac_picture);
 	}
 	
 	public void openTheatreSchool(View view){
-		openActivity(view.getContext(), R.raw.theatreschool, R.raw.rts_picture);
+		openActivity(view.getContext(), "-8Rrs3VbYBM", R.raw.rts_picture);
 	}
 	
 	public void openRyersonTheatre(View view){
-		openActivity(view.getContext(), R.raw.theatre, R.raw.the_picture);
+		openActivity(view.getContext(), "whBWjFQnYbU", R.raw.the_picture);
 	}
 	
 	public void openArchitectureBuilding(View view){
-		openActivity(view.getContext(), R.raw.architecture, R.raw.arc_picture);
+		openActivity(view.getContext(), "qVKjJVEcY-w", R.raw.arc_picture);
 	}
 	
 	public void openEricBuilding(View view){
-		openActivity(view.getContext(), R.raw.eric, R.raw.eph_she_picture);
+		openActivity(view.getContext(), "3I9LwBZx2zI", R.raw.eph_she_picture);
 	}
 	
 	public void openGeorgeBuilding(View view){
-		openActivity(view.getContext(), R.raw.george, R.raw.eng_picture);
+		openActivity(view.getContext(), "mvYhrvZtHMw", R.raw.eng_picture);
 	}
 	
 	public void openInteriorDesignBuilding(View view){
-		openActivity(view.getContext(), R.raw.interiordesign, R.raw.sid_picture);
+		openActivity(view.getContext(), "FyWyNNm0YtY", R.raw.sid_picture);
 	}
 	
 	public void openMonetaryTimes(View view){
-		openActivity(view.getContext(), R.raw.monetary, R.raw.mon_picture);
+		openActivity(view.getContext(), "TE0f5EBgvNk", R.raw.mon_picture);
 	}
 	
 	public void openRogersCentre(View view){
-		openActivity(view.getContext(), R.raw.rcc, R.raw.rcc_picture);
+		openActivity(view.getContext(), "IXCL5ZicMVg", R.raw.rcc_picture);
 	}
 	
 	public void openJorgensonHall(View view){
-		openActivity(view.getContext(), R.raw.jorgenson, R.raw.jor_picture);
+		openActivity(view.getContext(), "mQPlPB30oDw", R.raw.jor_picture);
 	}
 	
 	public void openLibrary(View view){
-		openActivity(view.getContext(), R.raw.library, R.raw.lib_picture);
+		openActivity(view.getContext(), "UrPWgULe0co", R.raw.lib_picture);
 	}
 	
 	public void openPodiumBuilding(View view){
-		openActivity(view.getContext(), R.raw.podium, R.raw.pod_picture);
+		openActivity(view.getContext(), "q8ExbI39Ajc", R.raw.pod_picture);
 	}
 	
 	public void openStudentLearningCentre(View view){
-		openActivity(view.getContext(), R.raw.studentlearning, R.raw.slc_picture);
+		openActivity(view.getContext(), "hL0sQNrwQFo", R.raw.slc_picture);
 	}
 	
 	public void openRyersonBookstore(View view){
-		openActivity(view.getContext(), R.raw.bookstore, R.raw.bks_picture);
+		openActivity(view.getContext(), "qKrGL7RNbHU", R.raw.bks_picture);
 	}
 	
 	public void openHeidenbergBuilding(View view){
-		openActivity(view.getContext(), R.raw.heidelberg, R.raw.hei_picture);
+		openActivity(view.getContext(), "m7HDPnXfFSk", R.raw.hei_picture);
 	}
 	
 	public void openImageArtsBuilding(View view){
-		openActivity(view.getContext(), R.raw.imagearts, R.raw.ric_picture);
+		openActivity(view.getContext(), "i6yScO1bO30", R.raw.ric_picture);
 	}
 	
 	public void openLakeDevo(View view){
-		openActivity(view.getContext(), R.raw.lakedevo, R.raw.devo_picture);
+		openActivity(view.getContext(), "P6kTGxDj_0o", R.raw.devo_picture);
 	}
 	
 	public void openTedRogers(View view){
-		openActivity(view.getContext(), R.raw.rogers, R.raw.trs_picture);
+		openActivity(view.getContext(), "60fuv4KZnqg", R.raw.trs_picture);
 	}
 	
 	public void openSouthBuilding(View view){
-		openActivity(view.getContext(), R.raw.southdond, R.raw.sbb_picture);
+		openActivity(view.getContext(), "izDo-Uc21jM", R.raw.sbb_picture);
 	}
 	
 	public void openStudentCampusCentre(View view){
-		openActivity(view.getContext(), R.raw.studentcampus, R.raw.scc_picture);
+		openActivity(view.getContext(), "5PnVfmibcR4", R.raw.scc_picture);
 	}
 	
 	public void openMattamyCentre(View view){
-		openActivity(view.getContext(), R.raw.mattamy, R.raw.mac_picture);
+		openActivity(view.getContext(), "nhmk4Ez1qfE", R.raw.mac_picture);
 	}
 	
-	public void openActivity(Context context, int video_name, int picture_name){
+	public void openActivity(Context context, String video_name, int picture_name){
 		intent = new Intent(context, BuildingsActivity.class);
 		intent.putExtra("video_name", video_name);
 		intent.putExtra("picture_name", picture_name);
